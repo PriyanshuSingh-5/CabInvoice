a3a9d5f02f4f6e5c23a3e3758aeb4f82f4ce8ec1
PriyanshuSingh-5 <80380590+PriyanshuSingh-5@users.noreply.github.com>
2021-05-19 18:22:43 +0530

    Update InvoiceSummary.cs

--- CabInVoice/InvoiceSummary.cs
+++ CabInVoice/InvoiceSummary.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace CabInVoice
 {
-    class InvoiceSummary
+   public class InvoiceSummary
     {
+        
+        public double totalFare;
+        public int noOfRides;
+        public double averageFarePerRide;
+
+        
+        // UC 3 Initializes a new instance of the <see cref="InvoiceSummary"/> class.
+        public InvoiceSummary(double totalFare, int noOfRides, double averageFairPerRide)
+        {
+            this.totalFare = totalFare;
+            this.noOfRides = noOfRides;
+            this.averageFarePerRide = averageFairPerRide;
+        }
+
+        
+        // Equalses the specified invoice summary
+        public override bool Equals(Object invoiceSummary)
+        {
+            if (invoiceSummary == null)
+            {
+                return false;
+            }
+            if (!(invoiceSummary is InvoiceSummary))
+            {
+                return false;
+            }
+            InvoiceSummary invoice = (InvoiceSummary)invoiceSummary;
+            return this.totalFare == invoice.totalFare && this.noOfRides == invoice.noOfRides && this.averageFarePerRide == invoice.averageFarePerRide;
+        }
+
+        
+        // Returns a hash code for this instance.
+        // A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        public override int GetHashCode()
+        {
+            return this.totalFare.GetHashCode() ^ this.noOfRides.GetHashCode() ^ this.averageFarePerRide.GetHashCode();
+        }
     }
 }
+
+
+
