4c20d81e1682b6655d1f91d714774d00a629565b
PriyanshuSingh-5 <80380590+PriyanshuSingh-5@users.noreply.github.com>
2021-05-19 18:18:31 +0530

    Update Ride.cs

--- CabInVoice/Ride.cs
+++ CabInVoice/Ride.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace CabInVoice
 {
-    class Ride
-    {
-    }
+    public double distance;
+        public int time;
+
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
 }
